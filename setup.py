8c7089f35fbf93d53c573e97ecf5dcbe9cddd290
Jakub Stawowy <kubas1989@gmail.com>
2017-04-07 22:24:49 +0200

    setup.py update

--- setup.py
+++ setup.py
@@ -21,17 +21,22 @@ setup(
     long_description=io.open('./README.rst', 'r', encoding='utf-8').read(),
     platforms='any',
     zip_safe=False,
-    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    # 'Development Status :: 4 - Beta',
     classifiers=[
                 'Programming Language :: Python',
                 'Programming Language :: Python :: 2',
                 'Programming Language :: Python :: 2.7',
                 'Programming Language :: Python :: 3',
-                'Programming Language :: Python :: 3.3',
                 'Programming Language :: Python :: 3.4',
                 'Programming Language :: Python :: 3.5',
-                'Programming Language :: Python :: 3.6'
+                'Programming Language :: Python :: 3.6',
+                'Development Status :: 5 - Production/Stable',
+                'Environment :: Web Environment',
+                'Framework :: Django',
+                'Framework :: Django :: 1.11',
+                'Framework :: Django :: 1.10',
+                'Framework :: Django :: 1.9',
+                'Framework :: Django :: 1.8',
+                'Intended Audience :: Developers'
     ],
     packages=find_packages(exclude=('tests',)),
     include_package_data=True,
